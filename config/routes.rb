6a3e86cf6e118b0698ecd333948057bb1abbcc4c
lpoole <keithpoole123@gmail.com>
2021-04-07 11:40:42 -0400

    updated new project to match old

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'static/pages'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: {format: :json} do
+    resource :user, only: [:create, :index, :show]
+    resource :session, only: [:create, :destroy, :show]
+    
+  end
+
+  root "static_pages#root"
 end
